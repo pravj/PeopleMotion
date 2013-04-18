fe6d55ec5f33f387ae8d0f4eb92387cda1a6fe44
Pravendra Singh <hackpravj@gmail.com>
2013-04-18 20:24:01 +0530

    main script for that

---
+++ process.js
@@ -0,0 +1,83 @@
+/*Hi there...nice to meet YOU specially HERE*/
+      function mouseCord(event) {
+        return {
+          x: event.clientX,
+          y: event.clientY
+        };
+      } 
+      function rangeX(a,x,b)
+      {
+        if(x>a && x<=b)
+          {return 1;}
+        else
+          {return 0;}
+      }
+      function rangeY(a,y,b)
+      {
+        if(y>a && y<=b)
+          {return 1;}
+        else 
+          {return 0;}
+      }
+      /***********detected :)*************/
+      /****************activities on mousemove*******************/
+      document.addEventListener('mousemove', function(event) {
+        /* page dimensions */ 
+        //var w = document.body.offsetWidth;
+  var w = window.innerWidth;
+        var h = window.innerHeight;
+	//document.getElementById("navigation-bar").innerHTML=w+" alpha "+h;
+        /*******************/
+        /*current co-ordinates*/
+        var where = mouseCord(event); 
+        var X = where.x;
+        var Y = where.y;
+        var current = 'Mouse Position: ' +X+','+Y;
+        /***********to find the region and change accordingly************/
+        /* wait! something is happening */
+        var id = 1;
+        for(id=1;id<=28;id++)
+        {var obj = $('img[name='+id+']');
+        var objCord = obj.offset();
+        var objX = objCord.left;
+        var objY = objCord.top;
+        if((rangeX(objX,X,objX+86)==1) && (Y>=objY+100))/*bottom*/
+        {
+          obj.attr('src','http://www.ekshitij.com/media/images/output2/'+id+'/3.JPG');
+        }
+        else if((rangeX(objX+86,X,w)==1) && (rangeY(objY,Y,objY+100)==1))/*right*/
+        {
+          obj.attr('src','http://www.ekshitij.com/media/images/output2/'+id+'/5.JPG');
+        }
+        else if((rangeX(0,X,objX)==1) && (rangeY(objY,Y,objY+100)==1))/*left*/
+        {
+          obj.attr('src','http://www.ekshitij.com/media/images/output2/'+id+'/4.JPG');
+        }
+        else if((rangeX(objX,X,objX+86)==1) && (rangeY(0,Y,objY)==1))/*just upside*/
+        {
+          obj.attr('src','http://www.ekshitij.com/media/images/output2/'+id+'/2.JPG');
+        }
+        else if((rangeX(objX,X,objX+86)==1) && (rangeY(objY,Y,objY+100)==1))/*on image*/
+        {
+          obj.attr('src','http://www.ekshitij.com/media/images/output2/'+id+'/1.JPG');
+        }
+        else if((rangeX(objX+86,X,w)==1) && (rangeY(0,Y,objY)==1))/*right-up-corner*/
+        {
+          obj.attr('src','http://www.ekshitij.com/media/images/output2/'+id+'/6.JPG');
+        }
+        else if((rangeX(0,X,objX)==1) && (rangeY(0,Y,objY)==1))/*left-up-corner*/
+        {
+          obj.attr('src','http://www.ekshitij.com/media/images/output2/'+id+'/7.JPG');
+        }
+        else if((rangeX(0,X,objX)==1) && (rangeY(objY+86,Y,h)==1))/*left-down-corner*/
+        {
+          obj.attr('src','http://www.ekshitij.com/media/images/output2/'+id+'/8.JPG');
+        }
+        else if((rangeX(objX+86,X,w)==1) && (rangeY(objY+86,Y,h)==1))/*right-down-corner*/
+        {
+          obj.attr('src','http://www.ekshitij.com/media/images/output2/'+id+'/9.JPG');
+        } 
+        }   
+        /* ok */
+      });
+    </script>
